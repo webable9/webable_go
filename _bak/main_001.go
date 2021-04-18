381e661eb0310d98d0826aea07d8b56279ed4d28
김석식 <webable9@gmail.com>
2021-04-18 04:08:51 +0000

    2021.04.18.

---
+++ _bak/main_001.go
@@ -0,0 +1,18 @@
+package main
+
+func main_001() {
+	var num1 int = 1
+	var num2 int = 2
+	
+	print("Hello goorm!")
+	print(num1)
+	print(num2)
+	print(num1 + num2)
+	print("Hello goorm!", num1 + num2,"\n")
+	
+	println("Hello goorm!")
+	println(num1)
+	println(num2)
+	println(num1 + num2)
+	println("Hello goorm!", num1 + num2)	
+}
---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a, b int = 10, 20
+    fmt.Println(a, b)
+    
+    i, j, k := 1, 2, 3
+    fmt.Println(i, j, k)
+    
+    var str1, str2 string = "Hello", "goorm"
+    fmt.Println(str1, str2)
+}
